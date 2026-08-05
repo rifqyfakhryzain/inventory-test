dcda5dc7d3fc36d23d7b48f78b49a6a5eb3d6325
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-08-05 09:53:14 +0700

    feat: aadd product dumy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(ProductSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
     }
 }
